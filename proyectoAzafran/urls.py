b4e4eb1c1363f0e55ffcb858392b15ed77a943bd
Yegua Celestial <yeguacelestial@gmail.com>
2020-03-27 15:31:54 -0600

    Created Maintenance page

--- proyectoAzafran/urls.py
+++ proyectoAzafran/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.maintainment, name='maintainment'),
+    #path('', views.home, name='home')
     path('admin/', admin.site.urls),
     path('azafran/', views.azafran),
 ]
--- proyectoAzafran/views.py
+++ proyectoAzafran/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Función principal
+def maintainment(request):
+    return render(request, 'mnt.html')
+    
 def home(request):
     return render(request, 'base.html')
 
