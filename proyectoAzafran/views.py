6dc7cad33ede85b59a1d58b4e6e89f59c09fee2e
Yegua Celestial <yeguacelestial@gmail.com>
2020-04-01 18:57:20 -0600

    Created index with animated background

--- proyectoAzafran/views.py
+++ proyectoAzafran/views.py
@@ -8,5 +8,8 @@ def maintainment(request):
 def home(request):
     return render(request, 'base.html')
 
+def index(request):
+    return render(request, 'index.html')
+    
 def azafran(request):
     return HttpResponse("view azafran")
