8696c98c3f14e8b01b2453ebc4de9fca7fa72406
Yegua Celestial <yeguacelestial@gmail.com>
2020-04-15 22:30:35 -0500

    Created style for content in paragraphs

--- proyectoAzafran/settings.py
+++ proyectoAzafran/settings.py
@@ -126,3 +126,30 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 # Configure Django App for Heroku
 import django_heroku
 django_heroku.settings(locals())
+
+# Setting up Node packages
+# STATICFILES_FINDERS = []
+
+# INSTALLED_APPS += (
+#     'compressor',
+#     'compressor_toolkit',
+# )
+
+# STATICFILES_FINDERS += (
+#     'compressor.finders.CompressorFinder',
+# )
+
+# COMPRESS_CSS_FILTERS = [
+#     'compressor.filters.css_default.CssAbsoluteFilter',
+#     'compressor.filters.cssmin.CSSMinFilter',
+#     'compressor.filters.template.TemplateFilter'
+# ]
+# COMPRESS_JS_FILTERS = [
+#     'compressor.filters.jsmin.JSMinFilter',
+# ]
+# COMPRESS_PRECOMPILERS = (
+#     ('module', 'compressor_toolkit.precompilers.ES6Compiler'),
+#     ('css', 'compressor_toolkit.precompilers.SCSSCompiler'),
+# )
+# COMPRESS_ENABLED = True
+# COMPRESS_OFFLINE = False
