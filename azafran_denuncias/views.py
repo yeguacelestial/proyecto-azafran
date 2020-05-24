ed9b0907a542210cf27652ea872388e5653fab3b
Yegua Celestial <yeguacelestial@gmail.com>
2020-05-24 16:10:36 -0500

    Created initial template for MDBootstrap testing

--- azafran_denuncias/views.py
+++ azafran_denuncias/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mdbootstrap(request):
+    return render(request, 'mdbootstrap.html')
--- azafran_project/urls.py
+++ azafran_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Custom
+from azafran_denuncias.views import mdbootstrap 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mdbootstrap) # Custom
 ]
