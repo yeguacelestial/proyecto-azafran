cbb7e0e61c5320bfafbef08949e02bee1ea842f8
Yegua Celestial <yeguacelestial@gmail.com>
2020-05-25 02:00:12 -0500

    Updated field tags for user

--- azafran_denuncias/forms.py
+++ azafran_denuncias/forms.py
@@ -5,4 +5,11 @@ class DenunciaForm(forms.ModelForm):
 
     class Meta:
         model = Denuncia
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ('edad', 'genero', 'escuela', 'denuncia')
+        labels = {
+            'edad': '¿Cuál es tu edad?',
+            'genero': '¿Cuál es tu genero?',
+            'escuela': '¿Perteneces a alguna de las siguientes instituciones?',
+            'denuncia': 'Alza la voz.'
+        }
