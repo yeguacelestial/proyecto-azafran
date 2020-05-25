91ed5333ed32ffaed6ed46d9ce29a27f63e6dd3f
Yegua Celestial <yeguacelestial@gmail.com>
2020-05-25 17:07:28 -0500

    Added toastr

--- azafran_denuncias/urls.py
+++ azafran_denuncias/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name='inicio'),
     path('denuncias', views.denuncias_list, name='denuncias'),
     path('alzalavoz', views.denuncias_form, name='denunciar'),
 ]
