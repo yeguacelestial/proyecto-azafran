ba4a0025d519ee3740a0df41160538161f039da1
Yegua Celestial <yeguacelestial@gmail.com>
2020-07-05 21:48:00 -0500

    Set project ready for Heroku deploy

--- azafran_project/settings.py
+++ azafran_project/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = 'azafran_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'AzafranDB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost'
-    }
-}
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     # }
+#     'default':{
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'AzafranDB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost'
+#     }
+# }
 
 #HEROKU DB
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
