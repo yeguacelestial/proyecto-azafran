b90206a3cb07a6dc99651409a8e89eced2114f0a
Yegua Celestial <yeguacelestial@gmail.com>
2020-07-07 21:00:28 -0500

    Created API structure and secured sensitive data

--- azafran_denuncias/urls.py
+++ azafran_denuncias/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . import views
+from azafran_denuncias.router import router # Required for API
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('testimonios/', views.DenunciasView.as_view(), name='denuncias'),
     path('alzalavoz', views.denuncias_form, name='denunciar'),
     path('testimonios/<int:pk>', views.DenunciaDetalladaView.as_view(), name='denuncia_detallada'),
+    path('api/', include(router.urls)), # Including API URL
 ]
--- azafran_project/settings.py
+++ azafran_project/settings.py
@@ -15,6 +15,8 @@ import os
 # NEW
 import django_heroku
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'azafran_denuncias',
     'crispy_forms',
+    'rest_framework'
 ]
 
 
@@ -82,24 +85,24 @@ WSGI_APPLICATION = 'azafran_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     # }
-#     'default':{
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'AzafranDB',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost'
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost'
+    }
+}
 
 #HEROKU DB
-DATABASES = {
-    'default': dj_database_url.config()
-}
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
