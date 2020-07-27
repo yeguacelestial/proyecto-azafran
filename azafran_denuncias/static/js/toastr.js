15cd88f56c5843d00c397910060134b548fba7a2
Yegua Celestial <yeguacelestial@gmail.com>
2020-07-27 16:30:12 -0500

    Modified toastr position

--- azafran_denuncias/static/js/toastr.js
+++ azafran_denuncias/static/js/toastr.js
@@ -1,33 +1,10 @@
-$('#telegram, #facebook').on('click', function(){
-  toastr.options = {
-    "closeButton": true,
-    "debug": false,
-    "newestOnTop": false,
-    "progressBar": true,
-    "positionClass": "toast-bottom-right",
-    "preventDuplicates": false,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-  }
-
-  toastr["info"]("Nos encontrarás en redes sociales próximamente.", "Expón a tu agresor")
-
-})
-
-$('#denuncias').ready(function(){
+$('#testimonios').on('click', function(){
   toastr.options = {
     "closeButton": false,
     "debug": true,
     "newestOnTop": true,
     "progressBar": true,
-    "positionClass": "toast-bottom-right",
+    "positionClass": "toast-bottom-full-width",
     "preventDuplicates": false,
     "onclick": null,
     "showDuration": "300",
