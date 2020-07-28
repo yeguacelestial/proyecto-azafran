15e441f9df92a0bdad616aebb94497b4b209430b
Yegua Celestial <yeguacelestial@gmail.com>
2020-07-27 20:27:34 -0500

    Modified migrations

--- azafran_denuncias/migrations/0002_auto_20200525_0736.py
+++ azafran_denuncias/migrations/0002_auto_20200525_0736.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('azafran_denuncias', '0001_initial'),
+        # ('azafran_denuncias', '0001_initial'),
     ]
 
     operations = [
