e147ee8e38738e483b378fd1ecae35ca7a478a5e
Yegua Celestial <yeguacelestial@gmail.com>
2020-07-27 21:38:02 -0500

    Updated migration

--- azafran_denuncias/migrations/0003_auto_20200727_2124.py
+++ azafran_denuncias/migrations/0003_auto_20200727_2124.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='denuncia',
             name='ip_publica_testimonio',
-            field=models.GenericIPAddressField(),
+            field=models.CharField(max_length=50),
         ),
     ]
